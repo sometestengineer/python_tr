4c55ada88d32cb699aad8cc4c9b0c335e789932d
ugot <you@yourdomain>
2016-10-09 11:29:19 -0700

    commit st test + few changes
    commit st test + few changes

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import time
+import unittest
+
 
 def is_alert_present(wd):
     try:
