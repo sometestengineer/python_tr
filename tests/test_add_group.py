cb1505c5958fc83738d7d6e52f650935a4d9a8d4
ugot <you@yourdomain>
2016-10-12 18:36:46 -0700

    created class grouphelper for group management

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -14,11 +14,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="g3", header="g3", footer="g33"))
+    app.group.create(Group(name="g3", header="g3", footer="g33"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
