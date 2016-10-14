4a822ea21292fc7828769eef7c615501ac7c2f89
ugot <you@yourdomain>
2016-10-13 19:57:01 -0700

    fixture modification

--- conftest.py
+++ conftest.py
@@ -2,9 +2,34 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture(scope='session')  # scope='session' - one fixture for all test in session,
+fixture = None
+
+
+@pytest.fixture  # (scope='session') - one fixture for all test in session,
 # example: one time browser starts for all tests
 def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)  # request = parameter with method addfinalizer
+    global fixture
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(username="admin", password="secret")
     return fixture
+
+
+@pytest.fixture(scope='session', autouse=True)  # without autouse browser won't close
+def stop(request):
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
+    request.addfinalizer(fin)  # request = parameter with method addfinalizer
+    return fixture
+
+
+# fixture for web application = browser with driver from which we can
+# interact with test system
+
+# fixture when working with db = connection with db,
+# we establish it and it's ready fo querying
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -3,12 +3,9 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="g3", header="g3", footer="g33"))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
+
